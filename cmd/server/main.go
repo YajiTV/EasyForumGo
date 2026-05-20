5bc616a81c59c402e3247c442da0b2e52b01c341
pmathys <mathys.paillekrobb@ynov.com>
2026-05-20 17:11:11 +0200

    feat: add minimal HTTP server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,23 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Forum is running"))
 	})
 
-	log.Printf("Server starting on :%s", port)
-	if err := http.ListenAndServe(":"+port, mux); err != nil {
-		log.Fatal(err)
-	}
+	log.Println("Server starting on :3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
