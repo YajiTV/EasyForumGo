d00cc79047e90d6ae142856ce8a9d071d6b1b671
pmathys <mathys.paillekrobb@ynov.com>
2026-05-21 14:23:05 +0200

    feat(3_posts) : update posts migrations table with date update_at

--- migrations/3_posts.sql
+++ migrations/3_posts.sql
@@ -3,8 +3,9 @@ CREATE TABLE IF NOT EXISTS posts (
     user_id    TEXT NOT NULL,
     title      TEXT NOT NULL,
     content    TEXT NOT NULL,
-    image_url  TEXT,
+    image_path  TEXT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
