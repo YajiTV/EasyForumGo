6503b730109a49acc5db9e09c9b62e76715c43b9
pmathys <mathys.paillekrobb@ynov.com>
2026-05-26 16:58:59 +0200

    fix(home): log error when fetching posts

--- internal/handler/home.go
+++ internal/handler/home.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 	"time"
@@ -48,6 +49,7 @@ func NewHomeHandler(db *sql.DB) *HomeHandler {
 func (h *HomeHandler) Home(w http.ResponseWriter, r *http.Request) {
 	posts, err := h.posts.GetAll()
 	if err != nil {
+		log.Printf("GetAll error: %v", err)
 		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
 		return
 	}
