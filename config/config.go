cf9c7460e831a97f1c1ab5c134df7df83ff3d75d
pmathys <mathys.paillekrobb@ynov.com>
2026-06-08 14:06:06 +0200

    feat: add OAuth configuration and database migration for users

--- config/config.go
+++ config/config.go
@@ -19,33 +19,37 @@ const (
 )
 
 type Config struct {
-	Port            string
-	HTTPSPort       string
-	TLSCertFile     string
-	TLSKeyFile      string
-	DBPath          string
-	MigrationsDir   string
-	StaticDir       string
-	TemplatesDir    string
-	UploadDir       string
-	MaxUploadBytes  int64
-	SessionDuration time.Duration
+	Port              string
+	HTTPSPort         string
+	TLSCertFile       string
+	TLSKeyFile        string
+	DBPath            string
+	MigrationsDir     string
+	StaticDir         string
+	TemplatesDir      string
+	UploadDir         string
+	MaxUploadBytes    int64
+	SessionDuration   time.Duration
+	OauthClientID     string
+	OauthClientSecret string
 }
 
 // Load loads the application configuration
 func Load() Config {
 	return Config{
-		Port:            stringEnv("PORT", defaultPort),
-		HTTPSPort:       stringEnv("HTTPS_PORT", defaultHTTPSPort),
-		TLSCertFile:     stringEnv("TLS_CERT_FILE", ""),
-		TLSKeyFile:      stringEnv("TLS_KEY_FILE", ""),
-		DBPath:          stringEnv("DB_PATH", defaultDBPath),
-		MigrationsDir:   stringEnv("MIGRATIONS_DIR", defaultMigrationsDir),
-		StaticDir:       stringEnv("STATIC_DIR", defaultStaticDir),
-		TemplatesDir:    stringEnv("TEMPLATES_DIR", defaultTemplatesDir),
-		UploadDir:       stringEnv("UPLOAD_DIR", defaultUploadDir),
-		MaxUploadBytes:  int64Env("MAX_UPLOAD_MB", defaultMaxUploadMegabytes) * 1024 * 1024,
-		SessionDuration: time.Duration(intEnv("SESSION_DURATION_H", defaultSessionDurationHours)) * time.Hour,
+		Port:              stringEnv("PORT", defaultPort),
+		HTTPSPort:         stringEnv("HTTPS_PORT", defaultHTTPSPort),
+		TLSCertFile:       stringEnv("TLS_CERT_FILE", ""),
+		TLSKeyFile:        stringEnv("TLS_KEY_FILE", ""),
+		DBPath:            stringEnv("DB_PATH", defaultDBPath),
+		MigrationsDir:     stringEnv("MIGRATIONS_DIR", defaultMigrationsDir),
+		StaticDir:         stringEnv("STATIC_DIR", defaultStaticDir),
+		TemplatesDir:      stringEnv("TEMPLATES_DIR", defaultTemplatesDir),
+		UploadDir:         stringEnv("UPLOAD_DIR", defaultUploadDir),
+		MaxUploadBytes:    int64Env("MAX_UPLOAD_MB", defaultMaxUploadMegabytes) * 1024 * 1024,
+		SessionDuration:   time.Duration(intEnv("SESSION_DURATION_H", defaultSessionDurationHours)) * time.Hour,
+		OauthClientID:     stringEnv("OAUTH_CLIENT_ID", ""),
+		OauthClientSecret: stringEnv("OAUTH_CLIENT_SECRET", ""),
 	}
 }
 
