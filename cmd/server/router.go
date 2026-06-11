8352da871b12ebbe5fcc173fffe4d8b47395f3c1
Palawi <bapcoloj@gmail.com>
2026-06-11 16:33:18 +0200

    feat(settings): add social preferences

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -61,6 +61,7 @@ func setupRouter(cfg config.Config, db *sql.DB, loginLimiter, writeLimiter *midd
 	mux.HandleFunc("GET /settings", settingsHandler.Show)
 	mux.Handle("POST /settings/email", writeLimiter.Wrap(http.HandlerFunc(settingsHandler.UpdateEmail)))
 	mux.Handle("POST /settings/password", writeLimiter.Wrap(http.HandlerFunc(settingsHandler.UpdatePassword)))
+	mux.Handle("POST /settings/social", writeLimiter.Wrap(http.HandlerFunc(settingsHandler.UpdateSocial)))
 	mux.Handle("POST /settings/delete", writeLimiter.Wrap(http.HandlerFunc(settingsHandler.DeleteAccount)))
 
 	// library routes
